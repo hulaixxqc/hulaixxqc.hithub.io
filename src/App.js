953cd3d1f238a478a41e60e3994cb1e1919e43c2
胡来 <hulai@hulaideMacBook-Pro.local>
2021-05-22 20:01:21 +0800

    c

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hoolay
+          Learn React hoolay 2
         </a>
       </header>
     </div>
